da7c3b74b336d815558e1ee156620dadd985481b
Leonardo Thibes <leonardothibes@gmail.com>
2018-04-24 17:08:18 -0300

    Configurando as referências

---
+++ Dotlib.Parser.Tests/RoleParserTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dotlib.Parser.Tests
+{
+    [TestClass]
+    public class RoleParserTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var parser = new RoleParser();
+        }
+    }
+}
--- Dotlib.Parser/RoleParser.cs
+++ Dotlib.Parser/RoleParser.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Dotlib.Parser
 {
     public partial class RoleParser
     {
-
+        public string ApplicationName { get; set; }
+        public List<string> Roles { get; set; }
     }
 
     public partial class RoleParser
